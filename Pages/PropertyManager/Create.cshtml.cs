6c5a5f4935130348194ed9daf8768d5015805356
Nigel <m0cvo@yahoo.co.uk>
2023-01-31 15:00:53 +0000

    Improved UI with label formatting

--- Pages/PropertyManager/Create.cshtml.cs
+++ Pages/PropertyManager/Create.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
 namespace CityBreaks.Pages.PropertyManager
 {
@@ -8,12 +10,16 @@ namespace CityBreaks.Pages.PropertyManager
         [BindProperty]
         public string Name { get; set; }
         [BindProperty]
+        [Display(Name = "Maximum Number of Guests")]
         public int MaxNumberOfGuests { get; set; }
         [BindProperty]
+        [Display(Name = "Day Rate")]
         public decimal DayRate { get; set; }
         [BindProperty]
+        [Display(Name = "Smoking Permitted")]
         public bool SmokingPermitted { get; set; }
         [BindProperty]
+        [Display(Name = "Available From")]
         public DateTime AvailableFrom { get; set; }
         public void OnGet()
         {

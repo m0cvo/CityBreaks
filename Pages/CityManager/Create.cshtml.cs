5fa8377112a3e1f391286aaf22d9dd2e560b5d39
Nigel <m0cvo@yahoo.co.uk>
2023-02-05 12:48:22 +0000

    Created a form to enable the uploading of image files for images of the cities. Also using extention validation to enable jpg only and provide limited security against vulnerabilities.

--- Pages/CityManager/Create.cshtml.cs
+++ Pages/CityManager/Create.cshtml.cs
@@ -1,12 +1,40 @@
+using CityBreaks.ValidationAttributes;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Primitives;
+using System.ComponentModel.DataAnnotations;
 
 namespace CityBreaks.Pages.CityManager
 {
     public class CreateModel : PageModel
     {
+        private readonly IWebHostEnvironment _environment;
+        public CreateModel(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
         [BindProperty]
-        public string CityName { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [BindProperty]
+        [Required]
+        [UploadFileExtensions(Extensions = ".jpg")]
+        public IFormFile Upload { get; set; }
+        [TempData]
+        public string Photo { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["Name"] = Name;
+                Photo = $"{Name.ToLower().Replace(" ", "-")}{Path.GetExtension(Upload.FileName)}";
+                var filePath = Path.Combine(_environment.WebRootPath, "images", "cities", Photo);
+                using var stream = System.IO.File.Create(filePath);
+                await Upload.CopyToAsync(stream);
+                return RedirectToPage("/CityManager/Index");
+            }
+            return Page();
+        }
     }
 }

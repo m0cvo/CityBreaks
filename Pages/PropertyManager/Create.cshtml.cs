7a60b1b45a686bae9340df2c6e5b827462625f23
Nigel <m0cvo@yahoo.co.uk>
2023-02-09 10:27:00 +0000

    Updated to RC1

--- Pages/PropertyManager/Create.cshtml.cs
+++ Pages/PropertyManager/Create.cshtml.cs
@@ -1,62 +1,46 @@
-using CityBreaks.Models;
-using CityBreaks.Services;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using System.ComponentModel.DataAnnotations;
-using System.Xml.Linq;
+using CityBreaks.Data;
+using CityBreaks.Models;
 
 namespace CityBreaks.Pages.PropertyManager
 {
     public class CreateModel : PageModel
     {
-        private readonly ICityService _cityService;
-        public CreateModel(ICityService cityService)
+        private readonly CityBreaks.Data.CityBreaksContext _context;
+
+        public CreateModel(CityBreaks.Data.CityBreaksContext context)
         {
-            _cityService = cityService;
-        }
-        [BindProperty]
-        public string Name { get; set; }
-        [BindProperty]
-        [Display(Name = "Maximum Number of Guests")]
-        public int MaxNumberOfGuests { get; set; }
-        [BindProperty]
-        [Display(Name = "Day Rate")]
-        public decimal DayRate { get; set; }
-        [BindProperty]
-        public Rating Rating { get; set; }
-        [BindProperty]
-        [Display(Name = "Smoking Permitted")]
-        public bool SmokingPermitted { get; set; }
-        [BindProperty]
-        [Display(Name = "Available From")]
-        public DateTime AvailableFrom { get; set; }
-        [BindProperty]
-        [Display(Name = "City")]
-        public int SelectedCity { get; set; }
-        public SelectList Cities { get; set; }
-        public string Message { get; set; }
-        public async Task OnGetAsync()
-        {
-            Cities = await GetCityOptions();
+            _context = context;
         }
 
-        public async Task OnPostAsync()
+        public IActionResult OnGet()
         {
-            Cities = await GetCityOptions();
-            if (ModelState.IsValid)
+        ViewData["CityId"] = new SelectList(_context.Cities, "Id", "Id");
+            return Page();
+        }
+
+        [BindProperty]
+        public Property Property { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
             {
-                var city = Cities.First(o => o.Value == SelectedCity.ToString());
-                Message = $"You selected {city.Text} with value of {SelectedCity}";
+                return Page();
             }
-        }
 
-        private async Task<SelectList> GetCityOptions()
-        {
-            var service = new SimpleCityService();
-            var cities = await service.GetAllAsync();
-            return new SelectList(cities, nameof(City.Id),
-         nameof(City.Name), null, "Country.CountryName");
+            _context.Properties.Add(Property);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
---
+++ Pages/PropertyManager/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CityBreaks.Data;
+using CityBreaks.Models;
+
+namespace CityBreaks.Pages.PropertyManager
+{
+    public class IndexModel : PageModel
+    {
+        private readonly CityBreaks.Data.CityBreaksContext _context;
+
+        public IndexModel(CityBreaks.Data.CityBreaksContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Property> Property { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Properties != null)
+            {
+                Property = await _context.Properties
+                .Include(m => m.City).ToListAsync();
+            }
+        }
+    }
+}

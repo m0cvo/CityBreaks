83e88de257c49f0da5fe267c141c580c72f80f24
Nigel <m0cvo@yahoo.co.uk>
2023-02-15 10:41:57 +0000

    added scripts using jQuery and then Fetch API to display further property details bound in partial Razor pages.

---
+++ Pages/Shared/_PropertyDetailsPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CityBreaks.Pages.Shared
+{
+    public class _PropertyDetailsPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Shared/_PropertyModalPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CityBreaks.Pages.Shared
+{
+    public class _PropertyModalPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
